908311531317e3c4bd1e485dc2a67b79d860bfa7
Hasatori <hradil.o@email.cz>
2021-03-15 22:25:25 +0100

    Collecting files for inspection is now called using progress indicator.

--- src/main/java/com/testspector/view/InspectTestAction.java
+++ src/main/java/com/testspector/view/InspectTestAction.java
@@ -3,17 +3,22 @@ package com.testspector.view;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.util.Computable;
+import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileVisitor;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
 import com.testspector.controller.TestspectorController;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class InspectTestAction extends AnAction {
@@ -25,20 +30,42 @@ public class InspectTestAction extends AnAction {
         Project project = anActionEvent.getProject();
         if (psiFile == null) {
             VirtualFile virtualFile = anActionEvent.getData(CommonDataKeys.VIRTUAL_FILE);
-            TestspectorController.initializeTestspector(project, collectPsiFilesFromVirtualFile(project,virtualFile), virtualFile.getCanonicalPath());
+            if (virtualFile != null) {
+                try {
+                    List<PsiFile> psiFiles = collectPsiFilesFromVirtualFile(project, virtualFile);
+                    TestspectorController.initializeTestspector(project, psiFiles, virtualFile.getCanonicalPath());
+                } catch (Exception ignored) {
+
+                }
+            }
         } else {
             TestspectorController.initializeTestspector(project, psiFile);
         }
     }
 
-    private List<PsiFile> collectPsiFilesFromVirtualFile(Project project,VirtualFile virtualFile) {
-        ArrayList<PsiFile> collectedPsiFiles = new ArrayList<>();
-        Arrays.stream(virtualFile.getChildren()).forEach(virtualFile1 -> {
-            PsiFile file = PsiManager.getInstance(project).findFile(virtualFile1);
-            if (file != null) {
-                collectedPsiFiles.add(file);
+    private List<PsiFile> collectPsiFilesFromVirtualFile(Project project, VirtualFile virtualFile) throws Exception {
+        return ProgressManager.getInstance().run(new Task.WithResult<List<PsiFile>, Exception>(project, "Collecting Files", true) {
+            @Override
+            protected List<PsiFile> compute(@NotNull ProgressIndicator indicator) {
+                return ApplicationManager.getApplication().runReadAction((Computable<List<PsiFile>>) () -> collectPsiFilesFromVirtualFile(indicator, project, virtualFile));
             }
-            collectedPsiFiles.addAll(collectPsiFilesFromVirtualFile(project,virtualFile1));
+        });
+    }
+
+    private List<PsiFile> collectPsiFilesFromVirtualFile(ProgressIndicator progressIndicator, Project project, VirtualFile virtualFile) {
+        ArrayList<PsiFile> collectedPsiFiles = new ArrayList<>();
+        progressIndicator.setText(virtualFile.getName());
+        VfsUtilCore.visitChildrenRecursively(virtualFile, new VirtualFileVisitor() {
+            @NotNull
+            @Override
+            public Result visitFileEx(@NotNull VirtualFile file) {
+                progressIndicator.setText(file.getName());
+                PsiFile psiFile = PsiManager.getInstance(project).findFile(file);
+                if (psiFile != null) {
+                    collectedPsiFiles.add(psiFile);
+                }
+                return CONTINUE;
+            }
         });
         return collectedPsiFiles;
     }
