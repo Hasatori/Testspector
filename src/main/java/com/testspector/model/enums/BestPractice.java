e9b9a0833d1d535b3db1295d9e34f8ffa57c6d4c
Hasatori <hradil.o@email.cz>
2021-03-20 16:46:53 +0100

    BestPractice links updated.

--- src/main/java/com/testspector/model/enums/BestPractice.java
+++ src/main/java/com/testspector/model/enums/BestPractice.java
@@ -17,13 +17,13 @@ import static com.testspector.model.utils.Constants.WEB_PAGE_BEST_PRACTICES_ULR;
 public enum BestPractice {
 
 
-    TEST_ONLY_PUBLIC_BEHAVIOUR("Test only public behaviour", "","testovat-pouze-veřejné-chování-testovaného-systému"),
-    AT_LEAST_ONE_ASSERTION("At least one assertion", "","minimálně-jedna-ověřovací-metoda-na-test"),
-    ONLY_ONE_ASSERTION("Only one assertion", "","právě-jedna-ověřovací-metoda-na-test"),
-    NO_GLOBAL_STATIC_PROPERTIES("No global static properties", "","nepoužívat-globální-statické-proměnné"),
-    SETUP_A_TEST_NAMING_STRATEGY("Setup a test naming strategy", "","určit-strategii-pojmenování-testů"),
-    CATCH_TESTED_EXCEPTIONS_USING_FRAMEWORK_TOOLS("Catch exceptions using frameworks tools", "","odchytávat-testované-výjimky-pomocí-nástrojů-knihoven-či-testovacích-frameworků"),
-    NO_CONDITIONAL_LOGIC("No conditional logic", "","podmíněná-logika");
+    TEST_ONLY_PUBLIC_BEHAVIOUR("Test only public behaviour", "","testovat-pouze-verejne-chovani-testovaneho-systemu"),
+    AT_LEAST_ONE_ASSERTION("At least one assertion", "","minimalne-jedna-overovaci-metoda-na-test"),
+    ONLY_ONE_ASSERTION("Only one assertion", "","prave-jedna-overovaci-metoda-na-test"),
+    NO_GLOBAL_STATIC_PROPERTIES("No global static properties", "","nepouzivat-globalni-staticke-promenne"),
+    SETUP_A_TEST_NAMING_STRATEGY("Setup a test naming strategy", "","urcit-strategii-pojmenovani-testu"),
+    CATCH_TESTED_EXCEPTIONS_USING_FRAMEWORK_TOOLS("Catch exceptions using frameworks tools", "","odchytavat-testovane-vyjimky-pomoci-nastroju-knihoven-ci-testovacich-frameworku"),
+    NO_CONDITIONAL_LOGIC("No conditional logic", "","podminena-logika");
 
     static {
         ONLY_ONE_ASSERTION.relatedRules = Collections.singletonList(AT_LEAST_ONE_ASSERTION);
@@ -40,10 +40,11 @@ public enum BestPractice {
         this.displayName = displayName;
         this.definition = definition;
         try {
-            this.webPageHyperlink = new URI(String.format("%s#%s",WEB_PAGE_BEST_PRACTICES_ULR,new String(webPageSection.getBytes(), StandardCharsets.UTF_8)));
+            this.webPageHyperlink = new URI(String.format("%s#%s",WEB_PAGE_BEST_PRACTICES_ULR,webPageSection));
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
+
     }
 
     public String getDisplayName() {
