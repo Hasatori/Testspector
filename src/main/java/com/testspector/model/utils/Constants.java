1f180a4a55520e4cc4cb3cd0dbfb87c231c4514e
Hasatori <hradil.o@email.cz>
2021-04-02 15:11:47 +0200

    Link to best practices updated to point to the google translated documentation. Wait time while inspection is running lowered to 1 sec. Version set to 1.0.1 and change notes updated.

--- src/main/java/com/testspector/model/utils/Constants.java
+++ src/main/java/com/testspector/model/utils/Constants.java
@@ -2,7 +2,7 @@ package com.testspector.model.utils;
 
 
 public final class Constants {
-    public static final String WEB_PAGE_BEST_PRACTICES_ULR = "https://github.com/Hasatori/Testspector/blob/master/doc/Practices.md";
+    public static final String WEB_PAGE_BEST_PRACTICES_ULR = "https://ooh3dpsdytm34sfhws63yjfbwy--github-com.translate.goog/Hasatori/Testspector/blob/master/doc/Practices.md";
 
 
     private Constants() {
