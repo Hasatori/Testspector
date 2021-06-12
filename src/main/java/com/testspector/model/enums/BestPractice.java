f6ac41c1ac78b7b6c9ee2cf8b9d293090043a772
Hasatori <hradil.o@email.cz>
2021-06-13 00:01:13 +0200

    Constants link updated to link github master, Best practices anchor link updated to english version

--- src/main/java/com/testspector/model/enums/BestPractice.java
+++ src/main/java/com/testspector/model/enums/BestPractice.java
@@ -17,13 +17,13 @@ import static com.testspector.model.utils.Constants.WEB_PAGE_BEST_PRACTICES_ULR;
 public enum BestPractice {
 
 
-    TEST_ONLY_PUBLIC_BEHAVIOUR("Test only public behaviour", "","testovat-pouze-verejne-chovani-testovaneho-systemu"),
-    AT_LEAST_ONE_ASSERTION("At least one assertion", "","minimalne-jedna-overovaci-metoda-na-test"),
-    ONLY_ONE_ASSERTION("Only one assertion", "","prave-jedna-overovaci-metoda-na-test"),
-    NO_GLOBAL_STATIC_PROPERTIES("No global static properties", "","nepouzivat-globalni-staticke-promenne"),
-    SETUP_A_TEST_NAMING_STRATEGY("Setup a test naming strategy", "","urcit-strategii-pojmenovani-testu"),
-    CATCH_TESTED_EXCEPTIONS_USING_FRAMEWORK_TOOLS("Catch exceptions using frameworks tools", "","odchytavat-testovane-vyjimky-pomoci-nastroju-knihoven-ci-testovacich-frameworku"),
-    NO_CONDITIONAL_LOGIC("No conditional logic", "","podminena-logika");
+    TEST_ONLY_PUBLIC_BEHAVIOUR("Test only public behaviour", "","test-only-the-public-behaviour-of-the-tested-system"),
+    AT_LEAST_ONE_ASSERTION("At least one assertion", "","at-least-one-assertion-per-test"),
+    ONLY_ONE_ASSERTION("Only one assertion", "","only-one-assertion-per-test"),
+    NO_GLOBAL_STATIC_PROPERTIES("No global static properties", "","do-not-use-global-static-properties"),
+    SETUP_A_TEST_NAMING_STRATEGY("Setup a test naming strategy", "","setup-a-test-naming-strategy"),
+    CATCH_TESTED_EXCEPTIONS_USING_FRAMEWORK_TOOLS("Catch tested exceptions using framework or library tools", "","catch-tested-exceptions-using-framework-or-library-tools"),
+    NO_CONDITIONAL_LOGIC("No conditional logic", "","do-not-use-if-switch-for-or-while-blocks-in-a-test");
 
     static {
         ONLY_ONE_ASSERTION.relatedRules = Collections.singletonList(AT_LEAST_ONE_ASSERTION);
