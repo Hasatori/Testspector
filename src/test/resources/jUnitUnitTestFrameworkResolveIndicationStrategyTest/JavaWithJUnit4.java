70627081188c143d8350049e3b103977b0865870
Hasatori <hradil.o@email.cz>
2021-06-19 16:37:39 +0200

    Support for more Jetbrains products added.

---
+++ src/test/resources/jUnitUnitTestFrameworkResolveIndicationStrategyTest/JavaWithJUnit4.java
@@ -0,0 +1,16 @@
+package com.baeldung.junit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SubstractionUnitTest {
+    Calculator calculator = new Calculator();
+
+    @org.junit.Test
+    public void substraction() {
+        assertEquals("substraction", 2, calculator.sub(5, 3));
+    }
+
+
+}
